bd3dba0523cdf196279d89b20b48decda2931e41
Thet Paing Soe <thetpaingsoe.ptn@gmail.com>
2025-07-03 19:42:29 +0700

    Added modal

---
+++ src/components/Modal.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Modal } from 'vue-neat-modal'
+
+const isOpen = ref(true)
+
+function openModal() {
+  isOpen.value = !isOpen.value
+  console.log(isOpen.value)
+}
+</script>
+
+<template>
+  <Modal v-model="isOpen" max-width="500px " class="bg-white">
+    <div class="bg-white p-4 rounded">
+      Modal Content
+
+      <button @click="isOpen = false">Close</button>
+    </div>
+  </Modal>
+
+  {{ isOpen }}
+  <button @click="openModal">Open Modal</button>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import 'vue-neat-modal/dist/vue-neat-modal.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
